a58b087a23725be6780c5488894b49e3c3ad955e
Eric <erich@fitplanapp.com>
2018-01-05 14:32:43 -0800

    Make script fail if Kafka times out

--- kafka_output.py
+++ kafka_output.py
@@ -6,6 +6,10 @@ TOPIC = "events"
 producer = KafkaProducer(bootstrap_servers=BOOTSTRAP_SERVERS)
 
 def save_messages(txt_messages):
+    last_future = None
     for msg in txt_messages:
-        producer.send(TOPIC, bytes(msg, "utf-8"))
-    producer.flush()
+        last_future = producer.send(TOPIC, bytes(msg, "utf-8"))
+
+    if last_future is not None:
+        last_future.get(timeout=20)
+        producer.flush()
